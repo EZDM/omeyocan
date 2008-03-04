a15217ad4e45007199dd78f4babb26c36c62107a
niccolo.cascarano <niccolo.cascarano@2db30c0b-ed46-0410-9931-399cb5748089>
2008-03-04 18:18:36 +0000

    Some GPL and fix the enter key on alert message in frame.php

--- trunk/lib/load.php
+++ trunk/lib/load.php
@@ -1,4 +1,27 @@
 <?PHP
+/*
+
+    This file is part of X7 chat Version 2.0.5 - RPG enhanced.
+    Released March 2008. Copyright (c) 2008 by Niccolo' Cascarano.
+
+    X7 chat Version 2.0.5 - RPG enhanced is free software:
+     you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    X7 chat Version 2.0.5 - RPG enhanced is distributed 
+    in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with X7 chat Version 2.0.5 - RPG enhanced.  
+    If not, see <http://www.gnu.org/licenses/>
+
+
+*/
 ///////////////////////////////////////////////////////////////
 //
 //		X7 Chat Version 2.0.5
