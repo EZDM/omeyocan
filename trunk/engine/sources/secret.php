834fa30f8393d31f62b5133b725a3a4d3fc659ea
niccolo.cascarano <niccolo.cascarano@2db30c0b-ed46-0410-9931-399cb5748089>
2009-01-27 19:04:54 +0000

    Now the secret moves

--- trunk/engine/sources/secret.php
+++ trunk/engine/sources/secret.php
@@ -2,10 +2,35 @@
 
 function secret_main(){
     global $print,$x7c,$x7s,$prefix,$db;
-    if(isset($_GET['code']) && $_GET['code']=="2621"){
+
+
+    if($_GET['secret']=='')
+        die("Empty secret");
+
+    
+        
+    $query = $db->DoQuery("SELECT name FROM {$prefix}rooms WHERE long_name='{$_GET['secret']}'");
+    $row = $db->Do_Fetch_Assoc($query);
+
+
+    include("./lib/secrets/{$_GET['secret']}.php");
+
+    //It changes each 65535 seconds ~18h
+    $today_secret=$secrets[(time()>>16)%$secret_lenght];
+
+    if($row['name'] != $today_secret){
+          $db->DoQuery("UPDATE {$prefix}rooms SET name='$today_secret' WHERE long_name='{$_GET['secret']}'");
+          $db->DoQuery("UPDATE {$prefix}users SET position='$today_secret' WHERE position='{$row['name']}'");
+          $db->DoQuery("UPDATE {$prefix}messages SET room='$today_secret' WHERE room='{$row['name']}'");
+    }
+
+
+
+    
+    if(isset($_GET['code']) && $_GET['code']==$today_secret){
         $db->DoQuery("UPDATE {$prefix}users SET secrets='0' WHERE username='{$x7s->username}'");
         $body = '<img src="graphic/Giardino_dei_Suicidi.jpg">
-                  <a onClick="javascript: opener.location.href=\'index.php?act=frame&room=Giardino\'; window.close(self); ">
+                  <a onClick="javascript: opener.location.href=\'index.php?act=frame&room='.$today_secret.'\'; window.close(self); ">
                   
                   <div style="background-color: transparent; position: absolute; top: 380px; left: 465px; width: 100px; height: 40px;"></div>
 
