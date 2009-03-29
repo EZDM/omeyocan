2e8e4f3690803ad04cc43ac350cefc95f7945e58
niccolo.cascarano <niccolo.cascarano@2db30c0b-ed46-0410-9931-399cb5748089>
2009-03-29 23:32:01 +0000

    Some admin rights added

--- trunk/engine/lib/load.php
+++ trunk/engine/lib/load.php
@@ -353,7 +353,7 @@
 			$return['admin_objects']=$row[41];
 
 			// See if they should have access to the admin panel
-			$temp = $row[21]+$row[22]+$row[23]+$row[24]+$row[25]+$row[26]+$row[27]+$row[28]+$row[29]+$row[30]+$row[31]+$row[32]+$row[33]+$row[37]+$row[38];
+			$temp = $row[21]+$row[22]+$row[23]+$row[24]+$row[25]+$row[26]+$row[27]+$row[28]+$row[29]+$row[30]+$row[31]+$row[32]+$row[33]+$row[37]+$row[38]+$row[39]+$row[40]+$row[41];
 			if($temp != 0)
 				$return['admin_access'] = 1;
 
