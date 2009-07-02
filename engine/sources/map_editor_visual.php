3e9ef62b01fa3e4f0ee8389e5bd3da334181f969
niccolo.cascarano <niccolo.cascarano@2db30c0b-ed46-0410-9931-399cb5748089>
2009-07-02 15:51:56 +0000

    Added copyright to map_editor_visual.php

--- engine/sources/map_editor_visual.php
+++ engine/sources/map_editor_visual.php
@@ -1,3 +1,30 @@
+<?php
+/*
+
+This file is part of X7 chat Version 2.0.5 - RPG enhanced.
+Released March 2008. Copyright (c) 2008 by Niccolo' Cascarano.
+
+X7 chat Version 2.0.5 - RPG enhanced is free software:
+you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+X7 chat Version 2.0.5 - RPG enhanced is distributed
+in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with X7 chat Version 2.0.5 - RPG enhanced.
+If not, see <http://www.gnu.org/licenses/>
+
+
+*/
+
+?>
+
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
 <html>
   <head>
