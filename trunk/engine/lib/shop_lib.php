e8263ce53601dd6db5b569cf8a4004334146aa00
niccolo.cascarano <niccolo.cascarano@2db30c0b-ed46-0410-9931-399cb5748089>
2010-07-28 20:35:42 +0000

    Raised the maximum money transportable

--- trunk/engine/lib/shop_lib.php
+++ trunk/engine/lib/shop_lib.php
@@ -28,7 +28,7 @@
 $GLOBALS['max_items'] = 10;
 $GLOBALS['shopper'] = "_shopper_";
 $GLOBALS['money_name'] = "Cogs";
-$GLOBALS['money_group'] = 100;
+$GLOBALS['money_group'] = 500;
 $GLOBALS['money_group_size'] = 1;
 $GLOBALS['base_money'] = 100000;
 $GLOBALS['evaluate_cost'] = 10;
