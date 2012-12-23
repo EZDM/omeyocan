8d08c2306455661d665d533b694574194eaab17a
niccolo.cascarano@gmail.com <niccolo.cascarano@gmail.com@2db30c0b-ed46-0410-9931-399cb5748089>
2012-12-23 13:36:38 +0000

    New choose page

---
+++ trunk/engine/sources/register_intro.php
@@ -0,0 +1,121 @@
+<?PHP
+
+	$register_intro = '
+	21.12.2012<br>
+	Dopo tante chiacchiere sulla sua "fine" il mondo si aspettava qualcosa di piu\'
+	di T-Shirts e gadgets sui maya...Qualcuno blaterava sul cambio di era e il
+	risveglio delle anime, la venuta degli extraterrestri o il ritorno del figlio
+	di dio.
+	<br>
+	<br>
+	Beh alla fine avevano un po\' tutti ragione e un po\' tutti torto:
+	il mondo non e\' finito ma certamente ha cominciato un rapido declino verso
+	il baratro... e le anime si sono risvegliate, ma non sono certo quello che i
+	fanatici della new age si aspettavano. Niente hippies, niente Amore
+	universale, anzi...se possibile ancora piu\' odio e violenza.
+	Una Dimensione Oscura e intangibile si e\' accostata alla nostra realta\'  e
+	in alcune parti del mondo sta letteralmente invadendo la realta\' 
+	riversandoci dentro i suoi frutti, i suoi figli... Bestie immonde che si
+	nutrono di carne e sangue, che depredano non solo il corpo ma anche la mente
+	degli uomini. Li chiamano "Umbramorfi"...fatti di ombra, insomma.
+	<br>
+	<br>
+	Non credere che sia un evento inaspettato: i servizi segreti di vari governi
+	mondiali sono gia\'  a conoscenza di questa "Dimensione Oscura" e da diversi
+	anni stanno studiando un modo per interagire con essa, per capirla e
+	comprenderla. Diversi siti mondiali hanno avuto "lievi incidenti" con questa
+	Dimensione e i suoi effetti, tutti questi incidenti sono stati coperti con
+	situazioni create ad hoc, ad arte.
+	<br>
+	Senti qui:
+	<br>
+	"21 novembre 2012. esplosione sull\'autobus e\' avvenuta nel centro di Tel
+	Aviv, in viale Shaul ha-Melech, una delle strade piu\'  frequentate della
+	citta\', di fronte al quartier generale dell\'esercito israeliano. Ci
+	sarebbero una ventina di feriti, nessuno in modo grave. L\'attentato e\' 
+	stato rivendicato - con un breve comunicato diramato attraverso diversi siti
+	in lingua araba - dalle "Brigate dei martiri di Al Aqsa". Nella nota, l\'ala
+	militare della Jihad islamica sostiene che il terrorista che ha compiuto
+	l\'attacco è riuscito a fuggire."
+	<br>
+	<br>
+	Non esistono le brigate dei martiri di Al Aqsa, l\'attentato serviva a
+	distruggere il contenuto di una valigia contenuta sul pullman stesso,
+	operazione riuscita in pieno e senza troppi danni collaterali... 
+	(si intendo vittime).
+	<br>
+  <br>
+	Ma non e\' mica finita: 
+	<br>
+	"4 marzo 2012. Almeno 200 i morti e 1500 i feriti delle quattro esplosioni
+	succedutesi in un deposito di munizioni a Brazzaville capitale della
+	Repubblica del Congo. Tra questi anche alcuni cinesi, che al momento del
+	disastro stavano dormendo in un attiguo dormitorio aziendale. Le esplosioni
+	hanno causato danni anche nella vicina Kinshasa, la capitale della vicina
+	Repubblica Democratica del Congo. Inizialmente non era stata esclusa la
+	possibilita\' di un attentato come causa di quello che sembra sempre piu\' 
+	essere un incidente. Come possibili esecutori dell\'attentato si iniziava a
+	parlare dei seguaci di Pascal Lissouba, generale sconfitto nella guerra civile
+	del 1997 e in esilio a Parigi, ma la mancanza di rivendicazioni e le prime
+	indagini sembrano escludere questa possibilita\'..."
+	<br>
+	Anche stavolta l\'attentato serviva solamente a distruggere un sito
+	compromesso e non piu\' sicuro, una locazione X in cui esperimenti sulla
+	Dimensione Oscura e gli umbramorfi avevano preso un brutta piega... Se i
+	"danni collaterali" ti sembrano pazzeschi stavolta allora e\' meglio che non
+	te la fai nelle mutande baby: Muroroa te la ricordi? E la Marea nera del 4
+	agosto 2012 che si e\' riversata fuori dalla Deepwater Horizon te la ricordi?
+	Beh non era petrolio quel fluido scuro che ha contaminato l\'oceano...
+	era sangue.<br>
+	Sangue nero.<br>
+	<br>
+	E quanti altri luoghi sono oramai irrimediabilmente compromessi? Fottuti?
+	"Almeno dodici persone sono morte e quarantatre sono rimaste ferite in un
+	terribile incidente stradale, causato da un rimorchio che si e\'
+	accidentalmente sganciato da un camion ed ha urtato un autobus di linea che
+	stava portando al lavoro un gruppo di braccianti agricoli sulla strada
+	Alamo-Potrero nello stato di Veracruz, in Messico, nei pressi del confine con
+	Tamaulipas. L\'incidente e\' avvenuto alle 4.30 ora locale, ma sanitari e
+	vigili del fuoco sono ancora al lavoro per soccorrere i passeggeri del pullman
+	rimasti incastrati tra le lamiere."
+	Pff... per quanto ho sentito io su quell\'autobus "di linea" viaggiavano
+	alcuni agenti della CIA che si stavano dirigendo a Veracruz, beh come potrai
+	capire non ci sono arrivati, qualcuno ha sborsato un bel po\' di soldoni
+	affinche\' nessuno torni piu\' a Veracruz!
+	<br>
+	Cosa sia successo la\' e\' una cosa di cui non ho idea e se vuoi saperla tutta
+	nemmeno mi interessa.
+	<br>
+	<br>
+	Mi interessa pero\' sapere come sei arrivato qui a Gunkajima pero\',
+	perche\' se ti ci hanno mandato allora sara\'  meglio che mi confermi i tuoi
+	dati e posso procedere all\'identificazione: questo e\' uno di quei siti dove
+	abbiamo a che fare con la Dimensione Oscura e credimi... se questo posto e\'
+	chiamato Battle Island un motivo c\'e\'!
+	<br>
+	Se invece sei arrivato "per caso" beh...devi sapere che noi siamo un gruppo
+	militare apolitico e autonomo, la nostra forza e la segretezza: nessuno sa
+	della nostra esistenza, per questo ci chiamano GHOST - Fantasma. Se non sei
+	con noi allora non dovresti sapere della nostra esistenza, e quindi sei un
+	cadavere.
+	<br>
+	Voglio darti una possibilita\' pero\': se scegli di restare (e di vivere) puoi
+	essere addestrato a conoscere la Dimensione Oscura e a combatterla, non
+	abbiamo molti uomini qui e recentemente c\'e\' stato qualche "problemino
+	logistico di troppo"... una mano in piu\' farebbe comodo. Abbiamo alcuni
+	"civilI" che danno una mano, anche loro non hannoa vuto altre opzioni.
+	<br>
+	Considerando anche che le acque del circondario sono fottutamente inquinate
+	dalle radiazioni (non crederai davvero che Fukushima sia stato un incidente
+	vero?). Dubito che potrai tornare indietro considerando anche la brutta
+	tempesta in avvicinamento.
+	<br>
+	Capisco che decidere così su due piedi possa essere difficile, ma devi capire
+	che non hai altre scelte: oramai sei qui e non tornerai indietro vivo.
+	Beh sai come si dice dalle nostre parti?
+	<br>
+	<br>
+	Benvenuto per sempre.
+	';
+
+?>
