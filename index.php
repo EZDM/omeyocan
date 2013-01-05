36e5c47f763bf1b38477dd64ee9e907f0aa5491c
niccolo.cascarano <niccolo.cascarano@gmail.com>
2013-01-05 13:45:03 +0000

    New home

--- index.php
+++ index.php
@@ -1,144 +1,53 @@
-<?PHP
-	include_once('engine/lib/output.php');
-?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Omeyocan</title>
+<LINK REL="SHORTCUT ICON" HREF="./engine/favicon.ico">
 
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<script language="JavaScript" type="text/JavaScript">
+<!--
+function apriPopupCentrata(nome, titolo, lar, alt, feat){
+	var wdt = screen.width;
+	var hgt = screen.height;
+	var x = Math.round( (wdt / 2) - (lar / 2) );
+	var y = Math.round( (hgt / 2) - (alt / 2) );
+	window.open(nome, titolo, 'width=' + lar + ',height=' + alt + ',left=' + x + ',screenX=' + x + ',top=' + y + ',screenY=' + y +	',' + feat);
+}
 
-<html>
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-18911231-1']);
+  _gaq.push(['_trackPageview']);
 
-	<head>
-	<LINK REL="SHORTCUT ICON" HREF="./engine/favicon.ico">
-	  <title>Omeyocan</title>
-		<meta name="description" content="Gioco di ruolo on-line horror. Entra per 
-		sempre nell'oscurita'!" />
-
-		<style type="text/css">
-			body{
-				background-color: black;
-			}
-
-			div, td{
-				text-align: center;
-			}
-			
-			#enter{
-				position: absolute;
-				left: 380px;
-				top: 400px;
-				width: 220px;
-				height: 100px;
-			}
-			
-			#rules{
-				position: absolute;
-				top: 400px;
-				right: 0px;
-				width: 180px;
-				height: 100px;
-			}
-			
-			#hole{
-				position: absolute;
-				top: 400px;
-				left: 0px;
-				width: 230px;
-				height: 100px;
-			}
-			
-			#uncitizen{
-				position: absolute;
-				top: 180px;
-				left: 0px;
-				width: 300px;
-				height: 100px;
-			}
-			
-			#citizen{
-				position: absolute;
-				top: 180px;
-				right: 0px;
-				width: 230px;
-				height: 100px;
-			}
-
-			img {
-				border: 0;
-			}
-			
-			#main_img{
-				background-image: url('layout/homepage_dark.jpg');
-				width: 992px;
-				height: 576px;
-				position: relative;
-				
-			}
-
-			.center{
-				text-align: center;
-				margin-left: auto;
-				margin-right: auto;
-			}
-	
-			a:link img, a:visited img, a:hover img{
-				border: 0;
-				text-decoration: none;	
-			}
-			
-			.copyright{
-				font-size: 6pt;
-				color: grey;
-			}
-
-
-		</style>
-
-		<script type="text/javascript">
-
-		var _gaq = _gaq || [];
-		_gaq.push(['_setAccount', 'UA-18911231-1']);
-		_gaq.push(['_trackPageview']);
-
-		(function() {
+  (function() {
 		 var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
 		 ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
 		 var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-		 })();
-
-		</script>
-	</head>
-
-	<body>
-		<div>
-			<table class="center">
-				<tr>
-					<td colspan="2">
-					<div id="main_img">
-										
-					<a onClick="<?PHP echo popup_open(1028, 728, 'engine','main');?>" href="#"><div id="enter"></div></a>
-					
-					<a href="hole/"><div id="hole"></div></a>
-					<a href="manual/"><div id="rules"></div></a>
-					
-					<a onClick="<?PHP 
-						echo popup_open(484, 702, 'citizen.html','citizen');?>" href="#">
-						<div id="citizen"></div></a>
-					<a onClick="<?PHP
-						echo popup_open(484, 702, 'uncitizen.html','citizen'); ?>" href="#">
-						<div id="uncitizen"></div></a>
-					</div>
-					</td>
-				</tr>
-					
-			</table>
-		</div>
-		<object>
-		  <param name="bgcolor" value="#000000">
-			<param name="movie" width="0" height="0" value="./engine/graphic/letmecryhigh.swf">
-			<param name="quality" value="high">
-			<param name="allowScriptAccess" value="sameDomain">
-			<embed src="./engine/graphic/letmecryhigh.swf" width="0" height="0" quality="high" pluginspage="http://www.macromedia.com/go/getflashplayer" type="application/x-shockwave-flash" allowScriptAccess="sameDomain" bgcolor="#000000">
-			</embed>
-		</object>
-	</body>
-
-</html>
+  })();
+//-->
+</script>
+</HEAD>
+<BODY bgcolor="#000000">
+<div style="width:1024px; height:608px; margin: 0 auto;">
+<div style="width:992px; height:576px; margin: 0 auto; margin-top:16px">
+<OBJECT
+    classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000"
+	codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=5,0,0,0"
+	WIDTH="992" HEIGHT="576">
+	<PARAM NAME=movie VALUE="enter_def.swf">
+	<PARAM NAME=quality VALUE=high>
+	<PARAM NAME=bgcolor VALUE=#000000>
+ 	<EMBED
+		src="enter_def.swf"
+		quality=high
+		bgcolor=#000000
+		WIDTH="992"
+		HEIGHT="576"
+		TYPE="application/x-shockwave-flash"
+		PLUGINSPAGE="http://www.macromedia.com/shockwave/download/index.cgi?P1_Prod_Version=ShockwaveFlash">
+	</EMBED>
+</OBJECT>
+</div>
+</div>
+</BODY>
+</HTML>
