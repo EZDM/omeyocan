6790948d8bf82ebfb75b6e5970f1250496a94136
niccolo.cascarano <niccolo.cascarano@gmail.com>
2013-01-05 13:58:24 +0000

    New shop head.

--- engine/sources/roomlist.php
+++ engine/sources/roomlist.php
@@ -191,7 +191,7 @@ _gaq.push(['_trackPageview']);
 							// Create object
 							if(window.self.name == ''){
 								<?PHP
-									echo popup_open(1025, 725, '/engine','main'); ?>
+									echo popup_open(1026, 728, '/engine','main'); ?>
 								window.self.location.href='/courtesy.html';
 							}
 							if(window.self.name == 'sheet'){
