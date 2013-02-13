829dd53795c97585d247a9b3dedcbf44f7c4f858
niccolo.cascarano@gmail.com <niccolo.cascarano@gmail.com@2db30c0b-ed46-0410-9931-399cb5748089>
2013-02-13 11:17:13 +0000

    Cogs -> Kreds

--- trunk/engine/lib/shop_lib.php
+++ trunk/engine/lib/shop_lib.php
@@ -27,7 +27,7 @@
 
 $GLOBALS['max_items'] = 50;
 $GLOBALS['shopper'] = "_shopper_";
-$GLOBALS['money_name'] = "Cogs";
+$GLOBALS['money_name'] = "Kreds";
 $GLOBALS['money_group'] = 1000;
 $GLOBALS['money_group_size'] = 1;
 $GLOBALS['base_money'] = 100000;
