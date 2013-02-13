f38b0859d9e698faef95ea22f499c6388427d22b
niccolo.cascarano@gmail.com <niccolo.cascarano@gmail.com@2db30c0b-ed46-0410-9931-399cb5748089>
2013-02-13 11:22:09 +0000

    Fix typo

--- trunk/engine/sources/admin.php
+++ trunk/engine/sources/admin.php
@@ -2005,7 +2005,6 @@ function admincp_master(){
 			$body .= " <p style=\"text-align: center;\">";
 			$count = 0;
 			while ($row_category = $db->Do_Fetch_Assoc($query_category)) {
-				$body .= "<br>";
 				$count++;
 				$long_name = $row_category['category'];
 				if (!$row_category['category'])
@@ -2013,6 +2012,7 @@ function admincp_master(){
 				$body .= "<a href=\"index.php?act=adminpanel&cp_page=objects&category=".
 					$row_category['category']."\">[$long_name]</a>";
 				if($count % 5 == 0)
+					$body .= "<br>";
 			}
 
 			$body .= "</p>";
