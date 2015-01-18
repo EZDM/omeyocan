7c1afa071b2870f5d35655997d658f9e0df3e71c
niccolo.cascarano@gmail.com <niccolo.cascarano@gmail.com@2db30c0b-ed46-0410-9931-399cb5748089>
2015-01-18 22:01:09 +0000

    Fix intro text

--- trunk/engine/sources/register_intro.php
+++ trunk/engine/sources/register_intro.php
@@ -1,46 +1,18 @@
 <?PHP
 
 	$register_intro = '
-	<br>
-	<span style="display: table; margin: auto;">
-	&quot;Non isperate mai veder lo cielo<br>
-	i&#39; vegno per menarvi a l&#39;altra riva<br>
-	ne le tenebre etterne, in caldo e &#39;n gelo.&quot;<br><br>
-	[Commedia. Canto III. Verso 87]</span>
-
-	<br><br>
-	E venne una eclisse impensabile nel cielo: un tetro pianeta che si affacciava
-	tra le nuvole e che si faceva beffe della fisica non esercitando alcuna
-	gravit&#224; a terra, non scatenando il minimo maremoto.
+	&quot;Non temere questa oscurit&#224;, so che non vedi nulla... 
+	dammi solo la mano, ti porter&#242; dall&#39;altra parte, dobbiamo
+	andarcene da qui, non &#232; pi&#249; sicuro. Non fare rumore o i
+	demoni ci sentiranno.&quot;
+	<br>Vieni portato attraverso un tunnel  
+	immateriale e pulsante: stai lasciando la tua casa e tutte le 
+	cose a te familiari, stai entrando nella Dimensione Oscura. 
 	<br>
 	<br>
-	Un pianeta  che vibra in una frequenza diversa e che non interferisce con la
-	nostra fisica... Che meraviglioso scenario!
-  <br>
-	<br>
-	Ma le cose belle durano sempre poco e la calma di quel paesaggio insolito
-	&#232; stata spezzata dal vostro arrivo.
-	Vi chiamano uomini, ma per noi siete solo scimmie: bestie ignoranti e buone
-	solo come concime o schiavi.
-	Strano che siate arrivati ora che l&#39;Eterno Conflitto tra Cielo e Inferno
-	sia in stallo:
-	i mezzi sangue neph-El e beth-El sono incuriositi da voi e dai vostri
-	strumenti inconsueti vi accompagnate addirittura ed altri esseri mezzi
-	artificiali... golem pi&#249; evoluti?
-	<br>
-	<br>
-	Chiunque tu sceglierai di essere sappi che la citt&#224; di Tharsis &#232;
-	una fortezza dove i signori nephilim si sono arroccati per sfuggire
-	all&#39;Eterno Conflitto e a te non &#232; consentito l&#39;accesso per ora;
-	presto ci saranno altri scontri e altri spargimenti
-	di sangue, scegliti un luogo in cui nasconderti o un signore da servire;
-	scimmia,  uomo o mezzo sangue che sia non c&#39;&#232; posto qui per te.
-  <br>
-	<br>
-	Mi chiamo Caronte e sono il Traghettatore.
-	<br>
-	<br>
-	Benvenuto per sempre.
+	<a href="index.php?act=register&disclaimer_done">
+	&quot;Chi sei? Da dove vieni?&quot;
+	</a>
 	';
 
 ?>
