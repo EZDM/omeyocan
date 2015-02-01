0bceec2eaeafb679285904163e5f9af0501f3819
niccolo.cascarano@gmail.com <niccolo.cascarano@gmail.com@2db30c0b-ed46-0410-9931-399cb5748089>
2015-02-01 10:32:58 +0000

    radio gorilla

--- trunk/engine/sources/layout.php
+++ trunk/engine/sources/layout.php
@@ -145,7 +145,7 @@ function location_out(hoveritem) {
 			<div id="hintbtn" style="position: absolute; top: 672px; left: 283px;">
 			<a onClick="'.popup_open(446, 558, "index.php?act=hint", "hint").'"> 
 				<img src="./graphic/hint_button.gif" 
-				onMousemove="javascript: ShowPopup(event, this, \'Amypedia\');"
+				onMousemove="javascript: ShowPopup(event, this, \'Radio gorilla\');"
 				onMouseout="javascript: HidePopup(this);">
 			</a>
 			</div>';
